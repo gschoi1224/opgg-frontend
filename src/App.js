a8bc2f902a5eeefaeca215a5fb0438978af2153e
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2022-03-27 23:27:22 +0900

    FEAT: 기본 스타일 설정(리셋, 폰트)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import { Reset } from 'styled-reset';
 import MainPage from './pages/MainPage';
 import NotFoundPage from './pages/NotFoundPage';
 import SummonersPage from './pages/SummonersPage';
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/summoners/:id" element={<SummonersPage />} />
-            <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
+        <>
+            <Reset />
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/summoners/:id" element={<SummonersPage />} />
+                <Route path="/*" element={<NotFoundPage />} />
+            </Routes>
+        </>
     );
 }
 
